96c31a6b1daf674a5f5992e52605a21ac720153f
Ophélie FOUCAULT <foucault.ophelie.pro@gmail.com>
2020-05-11 11:33:29 +0200

    Noeud

---
+++ Donjon Procedural/Assets/Scripts/Noeud.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreativeSpore.SuperTilemapEditor;
+
+public class Noeud : MonoBehaviour
+{
+    public enum TYPE_DE_SALLE
+    {
+        CLOSE,
+        OPEN,
+        SECRET
+    };
+    public TilemapGroup salle;
+    public Vector2 position;
+    public Dictionary<Noeud, TYPE_DE_SALLE> liens;
+}
