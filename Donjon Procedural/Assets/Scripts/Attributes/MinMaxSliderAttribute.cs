cbf40682a9aacf2aed29e28b6fc00ec4696c98b1
Ophélie FOUCAULT <foucault.ophelie.pro@gmail.com>
2020-05-11 15:15:41 +0200

    MinMaxSlider

---
+++ Donjon Procedural/Assets/Scripts/Attributes/MinMaxSliderAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinMaxSliderAttribute : PropertyAttribute
+{
+    public float min;
+    public float max;
+
+    public MinMaxSliderAttribute(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
